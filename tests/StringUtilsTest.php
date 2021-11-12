9dcfc928024139a60814e67e78eb4d9b24253158
Ilya Zherebtsov <izherebcov@gmail.com>
2021-11-12 13:11:20 +0400

    new commit

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require("src/StringUtils.php");
+
+if (StringUtils\capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (StringUtils\capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+if (StringUtils\capitalize('') !== 1) {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
